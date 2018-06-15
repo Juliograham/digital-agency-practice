17383f59b309fe6530d5bebb80a5aae2d10c3315
Julio Graham <julioagraham@gmail.com>
2018-06-14 23:32:23 -0400

    Started building a law firm website showcased on Hermann's website

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.back_button').addEventListener('click', () => {
+    alert('Don\'t let this distract you from the fact that Philosoraptor was the best meme of all time.');
+});
+
+document.querySelector('.forward_button').addEventListener('click', () => {
+    alert('Fry from Futurama being unsure of things is second place.');
+});
+
